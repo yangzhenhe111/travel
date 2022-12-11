20bd7375e9175d1f14ff35b061b15dbd72bc7659
yangzhenhe111 <3258274495@qq.com>
2022-12-11 17:39:07 +0800

    测试jenkins

--- src/main/java/com/cy/test/TestController.java
+++ src/main/java/com/cy/test/TestController.java
@@ -2,9 +2,7 @@ package com.cy.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @ResponseBody
 @Controller
